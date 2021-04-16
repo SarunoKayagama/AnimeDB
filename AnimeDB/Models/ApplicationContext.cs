3ed49a32815620cbec7781fbc45c215069395f9a
Вячеслав Сальков <slavasalcov@mail.ru>
2021-04-16 09:09:57 +0300

    Переместил Контекст,, попытки в html для вьюшки основной таблички

--- AnimeDB/Models/ApplicationContext.cs
+++ AnimeDB/Models/ApplicationContext.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace AnimeDB.Data
 {
     public class ApplicationContext : DbContext 
@@ -11,6 +12,10 @@ namespace AnimeDB.Data
         public DbSet<AnimeDB.Models.Data.Anime> Animes { get; set; }
         public DbSet<AnimeDB.Models.Data.Author> Authors { get; set; }
         public DbSet<AnimeDB.Models.Data.Character> Characters { get; set; }
-
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            //Database.EnsureCreated();
+        }
     }
+}
 }
